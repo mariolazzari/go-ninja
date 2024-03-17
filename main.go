e975187cef7d37691c9b3cd7e00a6718a26dda02
Mario Lazzari <mario.lazzari@gmail.com>
2024-03-17 17:30:31 +0100

    Done lesson 3

--- main.go
+++ main.go
@@ -4,5 +4,17 @@ import "fmt"
 
 // entry point
 func main() {
-	fmt.Println("Ciao ciao")
+
+	var name string = "Mario"
+	var name2 = "test"
+	// var name3 string
+
+	name4 := "mario"
+
+	fmt.Println(name, name2, name4)
+
+	var age1 int = 20
+	var age2 int16 = 22
+
+	fmt.Println(age1, age2)
 }
